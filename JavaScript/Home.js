e8970d236ff01deb79d4036c32ee05b31bb062c6
BD-SERVICE-WALLET <154258073+BD-SERVICE-WALLET@users.noreply.github.com>
2023-12-19 00:28:16 +0600

    Add files via upload

---
+++ JavaScript/Home.js
@@ -0,0 +1,7 @@
+function myFunction(){
+    document.getElementById("demo").innerHTML = "Wow!";
+}
+
+function myFunction2(){
+    document.getElementById("secondId").innerHTML = "secondFunctionCalled";
+}
